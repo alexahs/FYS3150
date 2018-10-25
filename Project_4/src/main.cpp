b48fc5997f285288b6f81e63da84875fce6f8172
alexahs <alexanxer.harold.sexton@gmail.com>
2018-10-25 19:01:54 +0200

    started project 4

---
+++ Project_4/src/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cmath>
+#include <fstream>
+#include <iomanip>
+#include <cstdio>
+#include <chrono>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <time.h>
+#include <stdlib.h>
+
+
+using namespace std;
+
+
+int main(int argc, char* argv[])
+{
+
+  double 
+
+  double E = 0.0; //energy per "coupling constant" J
+
+  srand(time(NULL));
+
+  double a = (((double) rand() / (RAND_MAX)) > 0.5) ? 1: -1;
+
+  // for(int i = 0; i < n; i++)
+  // {
+  //   for(int j = 0; i < n; i++)
+  //   {
+  //     E += s[i]*s[j];
+  //   }
+  // }
+
+
+
+  return 0;
+}
