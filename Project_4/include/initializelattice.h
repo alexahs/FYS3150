8de140dae67db5c8c02d02498aa5e315e7f27827
alexahs <alexanxer.harold.sexton@gmail.com>
2018-11-21 19:50:16 +0100

    added some comments

--- Project_4/include/initializelattice.h
+++ Project_4/include/initializelattice.h
@@ -2,9 +2,9 @@
 #ifndef INITIALIZELATTICE_H
 #define INITIALIZELATTICE_H
 
-// inline int PeriodicBoundary( int i, int limit, int add );
 void InitializeLattice ( int dim, double **SpinMatrix, double &E, double &M, int ordered );
 
+//function for indexing periodic boundary conditions
 inline int PeriodicBoundary( int i, int limit, int add ) {
     return ( i + limit + add ) % ( limit );
 }
